7137f1937ec60db9970b60562e127a798ae26426
suraj <soorajkrpandit@gmail.com>
2022-06-21 14:12:27 +0530

    first commit

---
+++ es.js
@@ -0,0 +1,135 @@
+// function multiply(a, b = 2) {
+//   return a * b;
+// }
+
+// console.log(multiply(5));
+// // expected output: 10
+// --------------------------------------------------
+// var Template_Literals;
+// // In ES6, we can use a new syntax ${PARAMETER} inside of the back-ticked string.
+// let firstName = "smita";
+// let lastName = "shinde";
+// // console.log(`Your name is ${firstName} ${lastName}`);
+
+// console.log(`my name is ${firstName}`);
+
+
+// ------------------------------------------------------------
+// var Multi_line_Strings;
+
+// let poemData = `Johny Johny Yes Papa,
+//                 Eating sugar?  No, papa!
+//                 Telling lies? No, papa!
+//                 Open your mouth Ah, ah, ah`;
+
+// console.log(poemData)
+
+// ---------------------------------------------------------------
+// var spread_operator;
+
+// var arr1 = [0, 1, 2];
+// var arr2 = [3, 4, 5];
+// var arr3 = [...arr1, ...arr2]; // Append all elements in arr2 after arr1 and returnb
+
+// var arr3 = [...arr1, ...arr2];
+
+// // Equate to
+
+// console.log(arr3);
+// var arr4 = arr1.concat(arr2);
+
+// console.log(arr4)
+
+// var obj1 = {
+//   foo: "bar",
+//   x: 42,
+// };
+
+// var obj2 = {
+//   foo: "baz",
+//   y: 13,
+// };
+
+// var clonedObj = { ...obj2 };
+// console.log(clonedObj);
+// // // Cloned object: {foo: 'bar', X: 42}
+
+// var mergedObj = { ...obj1, .
+
+
+
+var destructuring_assignment;
+// it possible to unpack values from arrays, or properties from objects, into distinct variables.
+
+// let a, b
+// let arr = [10, 20];
+// let [a, b] = arr
+
+// console.log(b);
+// // // // expected output: 10
+
+// console.log(b);
+// // expected output: 20
+
+// let [a, b, , , ...rest] = [10, 20, 30, 40, 50];
+// console.log(a);
+// // // expected output: 10
+
+// console.log(b);
+// // // expected output: 20
+
+// console.log(rest);
+// // expected output: Array[30, 40, 50]
+
+// c
+
+
+let Modules;
+// modules with import and export operands.
+// We can import userID variable and getName method using import statement
+
+// export var userID = 10;
+// export function getName(name) {
+
+// };
+
+// // import { Console } from 'console';
+// import { userID, getName } from 'module';
+// console.log(userID); // 10
+
+
+// es7
+
+// ----------------------------------------------------------------
+var String_includes;
+// The includes() method returns true if a string contains a specified value, otherwise false:
+
+// let text = "Hello world, welcome to the universe.";
+// if (text.includes("world")) {
+//   console.log("yes i am here!!");
+// } else {
+//   console.log("NO i am not!!")
+// }
+
+
+var objKey;
+// Object.keys()
+
+// var obj = {
+//     foo: "bar",
+//     baz: 42
+// };
+
+// console.log(Object.keys(obj))
+// // // ["foo", "baz"]
+
+// var Objectvalues;
+// var obj2 = {
+//   foo: "bar",
+//   baz: 42,
+// };
+
+// console.log(Object.values(obj2));
+// ["bar", 42]
+
+
