73ecdbceeb5d09e096d3f05128068481618199d9
suraj <soorajkrpandit@gmail.com>
2022-06-21 15:30:26 +0530

    ES10_FEATURES ADDED

--- es.js
+++ es.js
@@ -1,77 +1,67 @@
-// Asynchronous iterator
-// Template string for non escape sequence
-// Regular expression reverse (look behind) assertion (this article)
-// Regular expression Unicode escape
-// Regular expression s/dotAll mode
-// Regular expression named capture group
-// Object expansion operator
-// Promise.prototype.finally
+// 5 new features of ES10 (2019)
+// Added the flat() method and flatMap() method of Array
+// Added trimStart() method and trimEnd() method of String
+// Object.fromEntries()
+// Symbol.prototype.description
+// String.prototype.matchAll
+// Function.prototype.toString() now returns exact characters, including spaces and comments
+// JSON⊂ECMAScript
+// Simplify try {} catch {} and modify the catch binding
+// New basic data type BigInt
+// globalThis
+// import()
+// Legacy RegEx
+// Private instance methods and accessors
 
 
-// -------------------------------------------------
+// -----------------------------------------------------------------------------
 
-// asynchronous iterator
-// ES2018 introduces asynchronous iterators, just like regular iterators, except that the next() method returns a Promise. Therefore, await can be used with the for...of loop to run asynchronous operations in a serial manner. For example:
+// flat()
 
-async function process(array) {
-  for await (let i of array) {
-    doSomething(i);
-  }
-}
-
-// template string of non escape sequence
-// Tags allow you to parse Template Strings with functions. The first parameter of the tag function contains an array of string values. The rest of the parameters are expression dependent. Finally, your function can return the processed string (or it can return something completely different).
-
-function foo(str) {
-    return str[0].toUpperCase();
-}
-
-foo`justjavac`; // Output JUSTJAVAC
-foo`Xyz`; // Output XYZ
-
-// regular expression named capture group
-const
- // reDate = /(?<year>[0-9]{4})-(?<month>[0-9]{2})-(?<day>[0-9]{2})/,
-  match  = reDate.exec('2018-04-30'),
-  year   = match.groups.year,  // 2018
-  month  = match.groups.month, // 04
-  day    = match.groups.day;   // 30
+var arr1 = [1, 2, [3, 4]];
+arr1.flat(); 
+// [1, 2, 3, 4]
  
-const
-  reDate = /(?<year>[0-9]{4})-(?<month>[0-9]{2})-(?<day>[0-9]{2})/,
-  d      = '2018-04-30',
-  usDate = d.replace(reDate, '$<month>-$<day>-$<year>');
-
-//   object expansion operator
-let a = [1,2,3];
-let b = [0, ...a, 4]; // [0,1,2,3,4]
+var arr2 = [1, 2, [3, 4, [5, 6]]];
+arr2.flat();
+// [1, 2, 3, 4, [5, 6]]
  
-let obj = { a: 1, b: 2 };
-let obj2 = { ...obj, c: 3 }; // { a:1, b:2, c:3 }
-let obj3 = { ...obj, a: 3 }; // { a:3, b:2 }
-let object = {
-  a: '01', b: '02'
-};
+var arr3 = [1, 2, [3, 4, [5, 6]]];
+arr3.flat(2);
+// [1, 2, 3, 4, 5, 6]
  
-let newObject = {
-  c: '03',
-  ...object
-};
+//Using Infinity as the depth, expand the nested array of any depth
+arr3.flat(Infinity); 
+// [1, 2, 3, 4, 5, 6]
  
-console.log(newObject); //{c: "03", a: "01", b: "02"}
+ 
+//Remove null items
+var arr4 = [1, 2, , 4, 5];
+arr4.flat();
+// [1, 2, 4, 5]
 
-// Promise.finally()
-// A Promise call chain either successfully reaches the last. then(), or fails to trigger. catch(). In some cases, you want to run the same code no matter whether Promise runs successfully or fails, such as clearing, deleting conversations, closing database connections, etc.
 
-function doSomething() {
-  doSomething1()
-  .then(doSomething2)
-  .then(doSomething3)
-  .catch(err => {
-    console.log(err);
-  })
-  .finally(() => {
-    // finish here!
-  });
-}
+// flatMap()
+
+var arr1 = [1, 2, 3, 4];
+
+arr1.map(x => [x * 2]); 
+// [[2], [4], [6], [8]]
+
+arr1.flatMap(x => [x * 2]);
+// [2, 4, 6, 8]
+
+// Only "flatten" the array returned by the function in flatMap
+arr1.flatMap(x => [[x * 2]]);
+// [[2], [4], [6], [8]]
+
+
+// added trimStart() method and trimEnd() method of String
+// String.trimStart() can be used to remove whitespace from the beginning of a string. String.trimEnd() can be used to remove whitespace from the end of a string.
+
+//let  greeting =  "    Hello World";
+console.log(greeting.trimStart());// "Hello World"
+
+let greeting = "Hello World    ";
+console.log(greeting.trimEnd());// "Hello World"
 
