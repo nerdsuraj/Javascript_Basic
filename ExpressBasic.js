cdc05b2dbd71e9c459fc6b0e8ec0067f073d8bae
suraj <soorajkrpandit@gmail.com>
2022-06-22 13:25:23 +0530

    bit change

--- ExpressBasic.js
+++ ExpressBasic.js
@@ -22,3 +22,7 @@
 // PATCH	   Update/Modify	
 
 // DELETE	   Delete
+
+
+//CORS IN EXPRESS
+// Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources.
