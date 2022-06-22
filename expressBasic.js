88bf276510eb956a0ea123d150a4873c6086d63e
suraj <soorajkrpandit@gmail.com>
2022-06-22 12:42:52 +0530

    new file added

---
+++ expressBasic.js
@@ -0,0 +1,24 @@
+// HTTP response status codes
+
+// HTTP response status codes indicate whether a specific HTTP request has been successfully completed. Responses are grouped in five classes:
+
+// 1.> Informational responses (100–199)
+// 2.> Successful responses (200–299)
+// 3.> Redirection messages (300–399)
+// 4.> Client error responses (400–499)
+// 5.> Server error responses (500–599)
+
+
+// Using HTTP Methods for RESTful Services
+
+// HTTP Verb	  CRUD	   Entire Collection (e.g. /customers)	       Specific Item (e.g. /customers/{id})
+
+// POST	        Create
+
+// GET	        Read
+
+// PUT	       Update/Replace
+
+// PATCH	   Update/Modify	
+
+// DELETE	   Delete
